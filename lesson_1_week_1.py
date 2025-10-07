f2b3f299fbf9fa6d3f5aa6034606860cc83edec0
JaveriaAli621 <26505941@edgehill.ac.uk>
2025-10-07 15:13:15 +0100

    Add files via upload

---
+++ lesson_1_week_1.py
@@ -0,0 +1,9 @@
+print("hello world")
+studentName="javeria ali"
+studentAge="18"
+print("Student name:" +studentName)
+print("Student Age:"+studentAge)
+n1=20
+n2=50
+total=n1+n2
+print("the sum is:" , total)
